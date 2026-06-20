647c8b372b427bd4a87ae772592f3b72720d1437
ramana399654-ux <ramana.399654@gmail.com>
2026-06-20 09:41:58 +0800

    Initial commit: Added mock backend and nginx config via WSL2

---
+++ backend/app.py
@@ -0,0 +1,6 @@
+def health_check():
+    """A simple function we can run a unit test against later"""
+    return "Healthy"
+
+if __name__ == "__main__"
+    print("Django API Server Simulation running....")
